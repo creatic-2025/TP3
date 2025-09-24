ea48998da049832f4e3785d22400e1b6821f39e8
creatic-2025 <bernaquezgirard@educ.college-montreal.qc.ca>
2025-09-24 10:55:40 -0400

    Initial commit, nothing fully works yet.

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ niveau_vie = 20
 force_adversaire = random.randint(1, 5)
 numero_adversaire = 0
 numero_combat = 0
-nombre_victiores = 0
+nombre_victoires = 0
 nombre_defaites = 0
+victoires_consecutives = 0
 
 
 def confrontation_monstre():
@@ -32,21 +33,93 @@ def combattre_monstre():
     global niveau_vie
     global numero_adversaire
     global numero_combat
+    global nombre_victoires
+    global nombre_defaites
+    global victoires_consecutives
     print("Vous testez vos chances avec le monstre...")
     numero_adversaire += 1
     numero_combat += 1
     des_nb = random.randint(1, 6)
-    print(f"Adversaire: {numero_adversaire}\nForce de l'adversaire: {force_adversaire}\nNiveau de vie de l'usager: {niveau_vie}\nCombat {numero_combat}: {nombre_victiores} victoires vs. {nombre_defaites} défaites.")
+    print(
+        f"Adversaire: {numero_adversaire}\nForce de l'adversaire: {force_adversaire}\n"
+        f"Niveau de vie de l'usager: {niveau_vie}\n"
+        f"Combat {numero_combat}: {nombre_victoires} victoires vs. {nombre_defaites} défaites.")
     print(f"Lancer du dé: {des_nb}")
-    time.sleep(1)
+    time.sleep(2)
     if des_nb >= force_adversaire:
-        print("Vouz tué le monstre, voux gagnez 4 points de vie.")
-        niveau_vie += 4
+        print(f"Dernier combat: Victoire")
+        time.sleep(1)
+        print(f"Vous tué le monstre, voux gagnez {force_adversaire} points de vie.")
+        niveau_vie += force_adversaire
         print(f"Vous avez {niveau_vie} points de vie.")
+        nombre_victoires += 1
+        victoires_consecutives += 1
+        print(f"Vous avez {victoires_consecutives} de victoires consécutives.")
     if des_nb < force_adversaire:
-        print("Vous vouz faite blesser par le monstre, vous perdez 4 points de vie.")
-        niveau_vie -= 4
+        print(f"Dernier combat: Défaite")
+        print(f"Vous vous faite blesser par le monstre, vous perdez {force_adversaire} points de vie.")
+        niveau_vie -= force_adversaire
         print(f"Vous avez {niveau_vie} points de vie.")
+        nombre_defaites = + 1
+        print(f"Dommage, vos victoires consécutives sont remis à 0.")
+        victoires_consecutives = 0
+
+
+def boss_fight():
+    global niveau_vie
+    global numero_adversaire
+    global numero_combat
+    global nombre_victoires
+    global nombre_defaites
+    global victoires_consecutives
+    print("Vous rencontrez un boss, vouz sentez en trouble...")
+    numero_adversaire += 1
+    numero_combat += 1
+    des_nb_boss = random.randint(1, 6)
+    force_adversaire_boss = random.randint(1, 5)
+    niveau_vie_boss = 10
+    print(
+        f"Adversaire: {numero_adversaire}\nForce de l'adversaire: {force_adversaire}\n"
+        f"Niveau de vie de l'usager: {niveau_vie}\n"
+        f"Combat {numero_combat}: {nombre_victoires} victoires vs. {nombre_defaites} défaites.")
+    print(f"Lancer du dé: {des_nb_boss}")
+    time.sleep(2)
+    if des_nb_boss >= force_adversaire_boss:
+        print(f"Dernier attaque: Succès")
+        time.sleep(1)
+        print(f"Vous avez endommagé le boss, il perd {des_nb_boss} points de vie")
+        niveau_vie_boss -= des_nb_boss
+        print(f"Le boss a maintenant {niveau_vie_boss} points de vie")
+    if des_nb_boss < force_adversaire_boss:
+        print("Dernier attaque: Dommage")
+        time.sleep(1)
+        print(f"Le boss vous blesse, vous perdez {des_nb_boss} de vie.")
+
+
+def contourner_monstre():
+    global niveau_vie
+    global numero_adversaire
+    print("Vouz contournez le monstre et vous tordez vos cheviles, vouz perdez un point de vie.")
+    niveau_vie -= 1
+    numero_adversaire += 1
+
+
+def regles_jeu():
+    print(
+        "Voici les règles:"
+        "\nPour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l'adversaire. "
+        "Dans ce cas, le niveau de l'usager est augmenté de la force de l'adversaire."
+        "\nUne défaite a lieu lorsque la valeur du dé lancé est inférieur ou égale à la force de l'adversaire. "
+        "Dans ce cas, le niveau de l'usager est diminué de la force de l'adversaire.")
+    print("\nLa partie se termine lorsque les points de vie de l'usager tombe sous 0")
+    print(
+        "\nL'usager peut combattre ou éviter chaque adversaire, dans le cas d'évittement, "
+        "il y a une penalité de 1 point de vie.")
+
+
+def quitter():
+    print("Merci et au revoir...")
+    time.sleep(2)
 
 
 while game_play:
@@ -56,3 +129,16 @@ while game_play:
     if choix == "1":
         combattre_monstre()
         time.sleep(2)
+    if choix == "2":
+        contourner_monstre()
+        time.sleep(2)
+    if choix == "3":
+        regles_jeu()
+        time.sleep(2)
+    if choix == "4":
+        quitter()
+        game_play = False
+    if niveau_vie == 0:
+        print(f"Vous êtes morts après une longue bataille. Vous avez battu {nombre_victoires} adversaires.")
+        quitter()
+        game_play = False
